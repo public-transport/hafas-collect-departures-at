92b0032347aca9079ce7fb69cb2efb6ad60b4722
Jannis R <mail@jannisr.de>
2017-12-10 02:05:42 +0100

    code style :shirt:

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ const createCollectDeps = (fetchDepartures) => {
 		throw new Error('fetchDepartures must be a function.')
 	}
 
-	const collectDeps = (id, _when) => {
+	const collectDeps = (id, initialWhen) => {
 		if ('string' !== typeof id || !validId.test(id)) {
 			throw new Error('id must be a string with 9 to 12 digits.')
 		}
-		_when = +_when
-		if ('number' !== typeof _when || Number.isNaN(_when) || _when === 0) {
-			throw new Error('when must be a Date or timestamp > 0.')
+		initialWhen = +initialWhen
+		if (
+			'number' !== typeof initialWhen ||
+			Number.isNaN(initialWhen) ||
+			initialWhen === 0
+		) {
+			throw new Error('when must be a valid Date or timestamp > 0.')
 		}
 
 		const makeIterator = () => {
-			let when = _when
+			let when = initialWhen
 
 			const iterate = (duration = 10) => {
 				return fetchDepartures(id, {when, duration})
@@ -30,10 +34,9 @@ const createCollectDeps = (fetchDepartures) => {
 							const w = +new Date(d.when)
 							return Number.isNaN(w) ? 0 : w
 						})
-						// console.error('old when', when, 'new when', +new Date(last.when))
 						when = +new Date(last.when)
 					}
-					// todo: find last, assign to when
+
 					return {done: false, value: deps}
 				})
 			}
